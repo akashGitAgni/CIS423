450b3af5b4a5ff8fc458c546ae920d2a7f684a06
Akash Agnihotri <akash.a.agnihotri@gmail.com>
2015-05-01 20:33:34 -0700

    Adding server with Engine

--- WebServer/EngineServer.js
+++ WebServer/EngineServer.js
@@ -4,7 +4,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var PythonShell = require('python-shell');
 var http = require('http');
-
+var fs = require('fs');
 var app = express();
 
 var server = http.createServer(app);
@@ -13,43 +13,81 @@ app.set('views', path.join(path.resolve(path.dirname()), 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+	extended : false
+}));
 
-//Routes
-/*****************************************************/
+// Routes
+/** ************************************************** */
 
 app.get('/home', function(req, res) {
-        res.render('home.html');
+	res.render('Example.html');
 });
+
+
+app.get('/results', function(req, res) {
+	processResults(req,res);
+});
+
+
+function writeOuputFile(text) {
+
+	fs.writeFile("python/test-input.txt", text, function(err) {
+		if (err) {
+			return console.log(err);
+		}
+
+		console.log("The file was saved!");
+	});
+
+}
+
+
+function processResults(req,res)
+{
+
+	console.log("in app post");
+	var first = req.body.first;
+	var second = req.body.second;
+	var path = "test-input.txt";
+	var string = first + "\n" + second;
+	// writeOuputFile(string);
+
+	var options = {
+		mode : 'text',
+		scriptPath : './',
+		args : [ second, first ]
+	};
+
+	var pyshell = new PythonShell('engine.py', options);
+
+	pyshell.on('message', function(message) {
+		console.log("---------------------");
+		console.log(message);
+		res.end(message);
+	});
+
+	pyshell.end(function(err) {
+		if (err)
+			throw err;
+		console.log('finished');
+	});
+
+}
+
 
 app.post('/run', function(req, res) {
 
-        var first = req.body.first;
-        var second = req.body.second;
-
-        var pyshell = new PythonShell('test.py', {mode: 'text'});
-
-        pyshell.send(first);
-        pyshell.send(second);
-
-        pyshell.on('message', function(message) {
-                console.log(message);
-                res.end(message);
-        });
-
-        pyshell.end(function(err) {
-                if(err) throw err;
-                console.log('finished');
-        });
+	
 
 });
 
-/*****************************************************/
+/** ************************************************** */
 
 app.listen(3000, function() {
-        console.log('Listening on port 3000...');
+	console.log('Listening on port 3000...');
 });
 
-//server.listen(8080, '128.223.4.35');
+// server.listen(8080, '128.223.4.35');
 
 module.exports = app;
---
+++ WebServer/README.md
@@ -0,0 +1,17 @@
+###Accurp-Engine
+
+About
+-----
+Collection of machine learning methods to predict a translated text's accuracy.
+At the moment the Naive classifier is the onlyone suitable for predicitons. An update to the regression 
+predictor is pending, (convert it to a beta-regression-model).
+
+
+How to Use
+----------
+Please look at engine.py to see how to use the classifier. 
+
+
+Testing the Engine
+------------------
+Pending
