9ab98b0aa2ec97540a462229437fb21b31ee9120
Nathan Pointer <nathanpointer@128-223-223-66-wireless.uoregon.edu>
2015-05-03 11:11:04 -0700

    get, test and query functions written

--- CIS423ChromeExtension/app.js
+++ CIS423ChromeExtension/app.js
@@ -1,1 +1,29 @@
-console.log(document.all[0].outerHTML);
+
+
+getSource = function(){return $(document.all[0]).find("#source").val()};
+getTranslation = function(){
+	val = "";
+	$(document.all[0]).find("#result_box span").each(function(){
+		val += $(this).html() + " ";
+	});
+	return val;
+};
+
+
+// utility & testing functions
+setSource = function(val){
+	$(document.all[0]).find("#source").val(val);
+}
+
+testFunctions = function(){
+	console.log("getSource: ", getSource());
+	console.log("getTranslation: ", getTranslation());	
+}
+// testFunctions();
+
+queryValidator = function(){
+	return "validity:" + "10%";
+}
+
+
+alert( queryValidator(getSource(), getTranslation()) );
