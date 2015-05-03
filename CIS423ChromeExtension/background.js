0bb845bec057f95cffeffd43ce7ba474bf9f6a90
Brandenj <brandenj456@gmail.com>
2015-05-03 12:03:25 -0700

    button set up

--- CIS423ChromeExtension/background.js
+++ CIS423ChromeExtension/background.js
@@ -1,13 +1,31 @@
 chrome.browserAction.onClicked.addListener(function(tab){
-	var tabURL;
-	chrome.tabs.query({action: true, lastFocusedWindow: true}, (function(arrayOfTabs){
-		var activeTab = arrayOfTabs[0];
-		tabURL = activeTab.url;
-	}));
-	if (tabURL == "https://translate.google.com"){
-		console.log(tabURL);
-	}
-	else{
-		chrome.tabs.update({url: "https://translate.google.com"})
-	}
+	var tabURL = "about:black";
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, 
+			(function(arrayOfTabs){
+				var activeTab = arrayOfTabs[0];
+				//alert(activeTab.url + " ## ");
+				tabURL = activeTab.url;
+				//alert(tabURL);
+
+				if (CheckURL(tabURL) == true){
+					alert(tabURL);			//action when button is pushed and user is on google translate
+				}
+				else{
+					chrome.tabs.update({url: "https://translate.google.com"});
+				}
+
+			}
+		)
+	);
+
 });
+
+function CheckURL(testURL){
+	var tempURL = "https://translate.google.com/";
+	for (i = 0; i < tempURL.length; i++){
+			if (tempURL[i] != testURL[i]){
+				return false;
+			}
+	}
+	return true;
+};
